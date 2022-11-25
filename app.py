a4046c9a5b11984043b64a14e7f5061f9719422f
Josh Martin <josh.martin@cmtp.com.au>
2022-11-26 09:37:32 +1100

    Progerss- main api now has site, pie and bar information

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#################################################
 # Import all dependencies: 
 ################################################# 
 
@@ -11,11 +12,10 @@ from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify, redirect,url_for,render_template
 
-# Create connection to Hawaii.sqlite file
 #################################################
+# Database Setup
+##################################################
 
-# engine = create_engine("sqlite:///Users/udeshinpereira/Desktop/sqlalchemy-challenge/hawaii.sqlite")
-# engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 rds_connection_string = f'{config.protocol}://{config.username}:{config.password}@{config.host}:{config.port}/{config.database_name}'
 engine = create_engine(rds_connection_string)
 
@@ -25,43 +25,45 @@ Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
 
-# #Save references to the measurement and station tables in the database
+# Save references to the sa3_table, shipment_table and australian_postcodes tables in the database
 SA3_table = Base.classes.sa3_table
 Postcodes = Base.classes.australian_postcodes
 Shipments = Base.classes.shipment_table
 
 #################################################
+# Flask Setup
+#################################################
+
 app = Flask(__name__)
 
-# Create Flask Routes 
 
-# Create root route
+#################################################
+# Flask Routes
+#################################################
+
+# //--------------------------- Create home route ---------------------------//
 @app.route("/")
 def home():
     return render_template("index.html")
+# //----------------------------//
 
+
+
+# //--------------------------- API - SA3 data ---------------------------//
 
 # Create a route that queries precipiation levels and dates and returns a dictionary using date as key and precipation as value
 @app.route("/api/sa3")
-def precipitation():
+def sa3():
     # Create our session (link) from Python to the DB
     session = Session(engine)
-
-    """Return a list of precipitation (prcp)and date (date) data"""
     
     # Create new variable to store results from query to Measurement table for prcp and date columns
     sa3_data = session.query(SA3_table.sa3).all()
 
     # Close session
     session.close()
-
-    # Create a dictionary from the row data and append to a list of all_passengers
-        # 1. Create an empty list of precipitation query values 
-        # 2. Create for loop to iterate through query results (precipitation_query_results) 
-        # 3. Create dictionary with key "precipitation" set to prcp from precipitation_query_results and key "date" to date from precipitation_query_results
-        # 4. Append values from precipitation_dict to your original empty list precipitation_query_values 
-        # 5. Return JSON format of your new list that now contains the dictionary of prcp and date values to your browser
     
+    # Create a dictionary from the row data and append to a list of tobs_12_months
     sa3_values = []
     for row in sa3_data:
         sa3_dict = {}
@@ -69,25 +71,193 @@ def precipitation():
         sa3_values.append(sa3_dict)
 
     return jsonify(sa3_values) 
+# //----------------------------//
 
 
-@app.route("/api/sites")
-def site():
+
+# //--------------------------- API - Site list ---------------------------//
+
+@app.route("/test")
+def test():
 
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    # Create new variable to store results from query to Measurement table for prcp and date columns
-    site_list = session.query(Shipments.smpPlantID).distinct()
+    # Create new variable to store results of unique site names
+    site_list = session.query(Shipments.smpPlantID).order_by(Shipments.smpPlantID)\
+                            .distinct()
 
-    sites = []
+    # Create new variable to store results of unique product types
+    products = session.query(Shipments.impPartGroupID).order_by(Shipments.impPartGroupID)\
+                            .distinct()
+
+    # Create new variable to store results
+    main = {}
+    bar = []
+
+    # Loop through each site in the site list
+    for site in site_list:
+        sites = {}
+        
+        data = []
+            # Loop through each product in the site list
+        for product in products:
+            products_dict = {}
+            periods = []
+            values = []
+
+
+            period_data = session.query(Shipments.ShipPeriod, func.sum(Shipments.quantityShipped)).\
+                filter(Shipments.smpPlantID == site[0]).\
+                filter(Shipments.impPartGroupID == product[0]).\
+                group_by(Shipments.ShipPeriod).\
+                order_by(func.sum(Shipments.quantityShipped).desc()).all()
+
+            for item in period_data:
+                periods.append(item[0])
+                values.append(item[1])
+
+            products_dict["Product"] = product[0]
+            products_dict["Period"] = periods
+            products_dict["Values"] = values
+            data.append(products_dict)
+
+
+
+        sites["site"] = site[0]
+        sites["Data"] = data
+
+        bar.append(sites)
+    main["Bar"] = bar
+
+
+    return jsonify(main)
+    # return f"{test}"
+
+# //----------------------------//
+
+# //--------------------------- Main API ---------------------------//
+
+@app.route("/main/")
+def main():
+
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+
+# //------------ Site list ------------ 
+    # Create new variable to store results of unique site names
+    site_list = session.query(Shipments.smpPlantID).order_by(Shipments.smpPlantID)\
+                            .distinct()
+
+    # Create new variable to store results
+    main = {}
+    site = []
+
+    # Loop through results and add values to a list
     for row in site_list:
-        site_dict = {}
-        site_dict["Site"] = row[0]
-        sites.append(site_dict)
+        item = row[0]
+        site.append(item)
 
-    return jsonify(sites)
+    # add Dictionary item to the main variable
+    main["Sites"] = site
 
-   
+
+# //------------ Pie data ------------ 
+
+    # Create new variable to store results
+    pie = []
+
+    # Loop through each site in the site list
+    for row in site_list:
+        sites = {}
+        products = []
+        units = []
+
+        # Create new variable to store results of data that is filtered to only display the itterations site name
+        # Data is also grouped by part type
+        data = session.query(Shipments.impPartGroupID, func.sum(Shipments.quantityShipped)).\
+                filter(Shipments.smpPlantID == row[0]).\
+                group_by(Shipments.impPartGroupID).\
+                order_by(func.sum(Shipments.quantityShipped).desc()).all()
+
+        # Loop through each product and append values into a list
+        for item in data:
+            products.append(item[0])
+            units.append(item[1])
+    
+        # add list item to the variables
+        sites["Products"] = products
+        sites["units"] = units
+        sites["site"] = row[0]
+        
+        # add Dictionary item to the pie variable
+        pie.append(sites)
+
+    # add Dictionary item to the main variable
+    main["Pie"] = pie
+
+
+# //------------ Bar data ------------ 
+
+    # Create new variable to store results of unique product types
+    products = session.query(Shipments.impPartGroupID).order_by(Shipments.impPartGroupID)\
+                            .distinct()
+
+    # Create new variable to store results
+    bar = []
+
+    # Loop through each site in the site list
+    for site in site_list:
+        sites = {}
+        
+        data = []
+
+        # Loop through each product in the site list
+        for product in products:
+            products_dict = {}
+            periods = []
+            values = []
+
+            # Create new variable to store results of data that is filtered to only display the itterations site name and product
+            # Data is also grouped by Shipped period
+            period_data = session.query(Shipments.ShipPeriod, func.sum(Shipments.quantityShipped)).\
+                filter(Shipments.smpPlantID == site[0]).\
+                filter(Shipments.impPartGroupID == product[0]).\
+                group_by(Shipments.ShipPeriod).\
+                order_by(func.sum(Shipments.quantityShipped).desc()).all()
+
+            # Loop through each period and append values into a list
+            for item in period_data:
+                periods.append(item[0])
+                values.append(item[1])
+
+            # add list item to the variables
+            products_dict["Product"] = product[0]
+            products_dict["Period"] = periods
+            products_dict["Values"] = values
+            data.append(products_dict)
+
+
+        # add list item to the variables
+        sites["site"] = site[0]
+        sites["Data"] = data
+
+
+        # add Dictionary item to the bar variable
+        bar.append(sites)
+    
+    # add Dictionary item to the main variable
+    main["Bar"] = bar
+
+
+
+    return jsonify(main)
+# //----------------------------//
+
+#################################################
+# Run App
+#################################################
+
 if __name__ == '__main__':
     app.run(debug=True)
