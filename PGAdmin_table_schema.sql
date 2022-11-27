6907f00e579fd91740510febdefdeb828e76e622
Josh Martin <josh.martin@cmtp.com.au>
2022-11-27 17:54:08 +1100

    updated table schema

--- PGAdmin_table_schema.sql
+++ PGAdmin_table_schema.sql
@@ -1,22 +1,15 @@
 -- drop table sa3_table;
--- drop table australian_postcodes;
 -- drop table shipment_table;
 
 CREATE TABLE "sa3_table" (
 	"sa3" INT,
 	"sa3name" VARCHAR(100),
+	"coorinates" double precision[],
 	"location_URI" VARCHAR(500),
+	"type" VARCHAR(50),
 	PRIMARY KEY (sa3)
 );
 
-CREATE TABLE "australian_postcodes" (
-	"postcode" INT,
-	"lat" FLOAT,
-	"long" FLOAT,
-	"sa3" Int,
-	"sa3name" VARCHAR(100),
-	PRIMARY KEY (postcode)
-);
 
 CREATE TABLE "shipment_table" (
 	id SERIAL PRIMARY KEY
@@ -36,13 +29,16 @@ CREATE TABLE "shipment_table" (
 	,"cmlAddressLine1" VARCHAR(100)
 	,"cmlCity" VARCHAR(50)
 	,"cmlState" VARCHAR(20)
-	,"cmlPostCode" INT
 	,"quantityShipped" FLOAT
 	,"ShipPeriod" VARCHAR(20)
 	,"ShipYear" VARCHAR(20)
 	,"ShipMonth" VARCHAR(20)
+	,"postcode" INT
+	,"lat" FLOAT
+	,"long" FLOAT
+	,"sa3" Int
+	,"sa3name" VARCHAR(100)
 );
 
 SELECT * FROM SA3_table;
-SELECT * FROM australian_postcodes;
 SELECT * FROM shipment_table;
